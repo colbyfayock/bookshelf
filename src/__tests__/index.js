54cc885c916888b250d2995b23bde8ae60765714
Kent C. Dodds <me+github@kentcdodds.com>
2020-05-19 17:17:09 -0600

    exercises/01-bootstrap

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,58 +1,15 @@
-import ReactDOM from 'react-dom'
-import {waitForElementToBeRemoved, screen, within} from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import {buildUser} from 'test/generate'
-
+import {screen} from '@testing-library/react'
 beforeAll(() => {
-  document.body.focus()
+  const root = document.createElement('div')
+  root.id = 'root'
+  document.body.append(root)
 })
 
-test('can login and use the book search', async () => {
-  // setup
-  const div = document.createElement('div')
-  div.setAttribute('id', 'root')
-  document.body.appendChild(div)
-  require('../index')
+test('renders the app', () => {
+  require('..')
 
-  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
-
-  const user = buildUser()
-
-  userEvent.click(screen.getByRole('button', {name: /register/i}))
-
-  const modal = within(screen.getByRole('dialog'))
-  await userEvent.type(modal.getByLabelText(/username/i), user.username)
-  await userEvent.type(modal.getByLabelText(/password/i), user.password)
-
-  userEvent.click(modal.getByRole('button', {name: /register/i}))
-
-  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
-
-  const discoverLink = screen.getAllByRole('link', {name: /discover/i})[0]
-  discoverLink.focus()
-  userEvent.click(discoverLink)
-
-  const searchInput = screen.getByPlaceholderText(/search/i)
-  await userEvent.type(searchInput, 'voice of war')
-
-  userEvent.click(screen.getByLabelText(/search/i))
-  await waitForElementToBeRemoved(() => screen.getAllByLabelText(/loading/i))
-
-  userEvent.click(screen.getByText(/voice of war/i))
-
-  expect(window.location.href).toMatchInlineSnapshot(
-    `"http://localhost/book/B084F96GFZ"`,
-  )
-
-  expect(
-    await screen.findByText(/to the west, a sheltered girl/i),
-  ).toBeInTheDocument()
-
-  userEvent.click(screen.getByRole('button', {name: /logout/i}))
-
-  expect(searchInput).not.toBeInTheDocument()
-
-  // cleanup
-  ReactDOM.unmountComponentAtNode(div)
-  document.body.removeChild(div)
+  screen.getByTitle('Bookshelf')
+  screen.getByRole('heading', {name: /Bookshelf/i})
+  screen.getByRole('button', {name: /Login/i})
+  screen.getByRole('button', {name: /Register/i})
 })
